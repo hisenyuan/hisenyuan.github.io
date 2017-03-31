c1ef5dbe2929339c64e1010aef919a23fc4e0b4c
hisenyuan <2811923@qq.com>
2017-03-31 18:13:44 +0800

    Site updated: 2017-03-31 18:13:38

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,17 +1,3 @@
-$(document).ready(function () {
-  var $headerInner = $('.header-inner');
-  var $sidebar = $('#sidebar');
-  var getSidebarTop = function(){
-    return $headerInner.height() + 10;
-  };
-  var setSidebarMarginTop = function(sidebarTop){
-    return $sidebar.css({ 'margin-top': sidebarTop });
-  };
-  var mql = window.matchMedia('(min-width: 991px)');
-  setSidebarMarginTop(getSidebarTop()).show();
-  mql.addListener(function(e){
-    if(e.matches){
-      setSidebarMarginTop(getSidebarTop());
-    }
-  });
-});
+// build time:Fri Mar 31 2017 18:13:34 GMT+0800 (中国标准时间)
+$(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var i=function(){return n.height()+10};var a=function(n){return r.css({"margin-top":n})};var e=window.matchMedia("(min-width: 991px)");a(i()).show();e.addListener(function(n){if(n.matches){a(i())}})});
+//rebuild by neat 
